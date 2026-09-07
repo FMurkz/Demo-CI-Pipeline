7f3e49beab0d25ce8fb44ff7d36489b2b3d8eeb9
FMurkz <feliciamurkes@gmail.com>
2026-09-07 14:54:39 +0200

    Bugs

--- src/app.py
+++ src/app.py
@@ -33,7 +33,7 @@ def add_item(item):
 
     # BUG
     existing = _run(
-        "SELECT id, name, quantity FROM items WHERE name = %s COLLATE utf8mb4_0900_ai_ci", #BUG: to fix, add, COLLATE utf8mb4_0900_ai_ci to make case insensitive
+        "SELECT id, name, quantity FROM items WHERE name = %s ", #BUG: to fix, add, COLLATE utf8mb4_0900_ai_ci to make case insensitive
         (name,),
         fetchone=True,
     )
@@ -56,7 +56,7 @@ def get_items():
 def mark_item_as_bought(item_id, bought=True):
     """Set an item's bought state and return it, or None if it does not exist."""
     #BUG: the parameters for the SQL query are in the wrong order, to fix: (bought, item_id)
-    _run("UPDATE items SET bought = %s WHERE id = %s", (bought, item_id), commit=True)
+    _run("UPDATE items SET bought = %s WHERE id = %s", (item_id, bought), commit=True)
     item = _run(
         "SELECT id, name, quantity, bought FROM items WHERE id = %s",
         (item_id,),
